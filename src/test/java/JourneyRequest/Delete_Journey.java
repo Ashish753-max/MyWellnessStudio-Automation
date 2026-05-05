d3810cf45972591e7894ebee6280dee87707e39e
Ashish.rai <ashishappnox1@gmail.com>
2026-05-05 10:25:42 +0530

    Implemented automation for the journey request section

---
+++ src/test/java/JourneyRequest/Delete_Journey.java
@@ -0,0 +1,57 @@
+package JourneyRequest;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Delete_Journey {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+ChromeDriver driver = new ChromeDriver();
+        
+        // maximize the window
+        driver.manage().window().maximize();
+        
+        // open the website 
+        driver.get("https://mydentalwellness-r897.vercel.app/");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        
+        // Click on the login button 
+        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@id=\"root\"]/div[2]/nav/div/div[2]/div/a[1]/button")));
+        loginButton.click();
+        
+        // Enter the email
+        WebElement email = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
+        email.sendKeys("ashishappnox1@gmail.com");
+        
+        // Enter the password
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("password"))).sendKeys("Ashish@567");
+       
+        
+        // click on login button
+        wait.until(ExpectedConditions.elementToBeClickable(
+				By.xpath("//*[@id=\"root\"]/div[2]/div[1]/div/form/button"))).click();
+        
+        // click on the Journey request section
+        wait.until(ExpectedConditions.elementToBeClickable(
+				By.xpath("//*[@id=\"root\"]/div[2]/aside/div[2]/div[1]/nav/div[3]"))).click();
+        
+        // click on the delete button of the journey 
+		wait.until(ExpectedConditions.elementToBeClickable(
+				By.xpath("//*[@id=\"root\"]/div[2]/div/main/div/div/div[2]/div[1]/table/tbody/tr[1]/td[6]/div/div/div[2]/button"))).click();
+		
+		 // click on the confirm delete button
+		wait.until(ExpectedConditions.elementToBeClickable(
+				By.xpath("//*[@id=\"root\"]/div[2]/div/main/div/div[2]/div[2]/div[2]/div/button[2]"))).click();
+        
+
+	}
+
+}
---
+++ src/test/java/JourneyRequest/Update_Journey.java
@@ -0,0 +1,51 @@
+package JourneyRequest;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Update_Journey {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+ChromeDriver driver = new ChromeDriver();
+        
+        // maximize the window
+        driver.manage().window().maximize();
+        
+        // open the website 
+        driver.get("https://mydentalwellness-r897.vercel.app/");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        
+        // Click on the login button 
+        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@id=\"root\"]/div[2]/nav/div/div[2]/div/a[1]/button")));
+        loginButton.click();
+        
+        // Enter the email
+        WebElement email = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
+        email.sendKeys("ashishappnox1@gmail.com");
+        
+        // Enter the password
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("password"))).sendKeys("Ashish@567");
+       
+        
+        // click on login button
+        wait.until(ExpectedConditions.elementToBeClickable(
+				By.xpath("//*[@id=\"root\"]/div[2]/div[1]/div/form/button"))).click();
+        
+        // click on the Journey request section
+        wait.until(ExpectedConditions.elementToBeClickable(
+				By.xpath("//*[@id=\"root\"]/div[2]/aside/div[2]/div[1]/nav/div[3]"))).click();
+		
+		// click on the edit button of the journey
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div[2]/div/main/div/div/div[2]/div[1]/table/tbody/tr[1]/td[6]/div/div/div[1]/button"))).click();
+
+	}
+
+}
