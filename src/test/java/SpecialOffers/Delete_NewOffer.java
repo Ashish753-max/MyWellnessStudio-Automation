76de384531c56f9aa4146a1c2f6dba1c614c910a
Ashish.rai <ashishappnox1@gmail.com>
2026-05-15 13:13:22 +0530

    implemented automation for the special offers section

---
+++ src/test/java/SpecialOffers/Delete_NewOffer.java
@@ -0,0 +1,55 @@
+package SpecialOffers;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Delete_NewOffer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+ChromeDriver driver = new ChromeDriver();
+        
+        // maximize the window
+        driver.manage().window().maximize();
+        
+        // open the website 
+        driver.get("https://mydentalwellness-r897.vercel.app/");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(50));
+        
+        // Click on the login button 
+        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//*[@id=\"root\"]/div[2]/nav/div/div[2]/div/a[1]/button")));
+        loginButton.click();
+        
+        // Enter the email
+        WebElement email = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
+        email.sendKeys("ashishappnox1@gmail.com");
+        
+        // Enter the password
+        WebElement password = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("password")));
+        password.sendKeys("Ashish@567");
+        
+        // click on login button
+        
+        WebElement login = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"root\"]/div[2]/div[1]/div/form/button")));
+        login.click();
+			
+		// Click on the Special offers section 
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div[2]/aside/div[2]/div[3]/nav/div[2]"))).click();
+		
+		//click on the delete button 
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div[2]/div/main/div/div/div[2]/div[1]/table/tbody/tr/td[6]/div/div/div[2]/button"))).click();
+		
+		// click on the confirm delete button
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div[2]/div/main/div/div[2]/div[2]/div[2]/div/button[2]"))).click();
+
+	}
+
+}
---
+++ src/test/java/SpecialOffers/Update_Offer.java
@@ -0,0 +1,10 @@
+package SpecialOffers;
+
+public class Update_Offer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
